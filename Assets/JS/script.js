2e2e220f50d1e113ac09fbcdfd549a7b7f8a9970
Mariah <mariahjohnston217@outlook.com>
2022-05-21 21:05:24 -0400

    linked reset css, css and js. created pseudo code steps for js

---
+++ Assets/JS/script.js
@@ -0,0 +1,13 @@
+/*
+step 1: create start button to start the game
+step 2: create a timer that starts when presented with the first question
+step 3: create if statement (if you answer question correctly, it will present another question and say "correct!")
+step 4: create if statement (if you answer question incorrectly, deduct time from timer and say "incorrect")
+step 5: clear interval when all questions are answered (stop the clock)
+step 6: when game is over "all done! your final score is ____."
+step 7: create textbox to insert initials at the end of the game
+step 8: create view highscore in top left to view highscore (use local storage)
+*/
+
+//variables
+
