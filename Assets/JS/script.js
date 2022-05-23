645b9ddc5b5a894d82187f453dc1d83a3896c06e
Mariah <mariahjohnston217@outlook.com>
2022-05-23 18:45:22 -0400

    added more pseudo code to js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,74 +1,81 @@
 /*
-step 1: create start button to start the game
-step 2: create a timer that starts when presented with the first question
-step 3: create if statement (if you answer question correctly, it will present another question and say "correct!")
-step 4: create if statement (if you answer question incorrectly, deduct time from timer and say "incorrect")
-step 5: clear interval when all questions are answered (stop the clock)
-step 6: when game is over "all done! your final score is ____."
-step 7: create textbox to insert initials at the end of the game
-step 8: create view highscore in top left to view highscore (use local storage)
+step 1: display start page, title, paragraph and start button
+step 2: timer will start when start button is clicked
+step 3: create start button to start the game and timer, display question    and 4 answer choices/buttons, start page is hidden, only display question page (after you select an answer, it will go to another page and hide previous question page)
+step 4.: create if statement (if you answer question correctly, it will present another question and say "correct!")
+step 5: create if statement (if you answer question incorrectly, deduct 10 seconds from timer and say "incorrect")
+step 6: clear interval when all questions are answered (stop the clock)
+step 7: when game is over "all done! your final score is ____." the time remaining on the clock is your final score.
+step 8: create textbox to insert initials at the end of the game
+step 9: create view highscore in top left to view highscore (use local storage)
 */
 
 //variables
 var startButton = document.querySelector(".start-button");
 var choices = document.querySelector("#choices");
 var timer;
-var timerCount; 
+var timerCount;
 var isAllDone = false;
 var finalScore = "";
 var highscores = 0;
 var questions = [
-    //The first question will be [0]
+    //reference: 
+    //questions.question[0]
+    //qustions.choiceA[0] etc
     {
         question: "Commonly used data types DO NOT include: ",
-        choiceA: "strings", 
+        choiceA: "strings",
         choiceB: "booleans",
         choiceC: "alerts",
         choiceD: "numbers",
         answer: "C"
-    }
-    //The second question will be [1]
+    },
+    //reference: 
+    //questions.question[1]
+    //questions.choiceA[1] etc
     {
         question: "The condition of and if/else statement is enclosed with___.",
-        choiceA: "quotes", 
+        choiceA: "quotes",
         choiceB: "curly brackets",
         choiceC: "parenthesis",
         choiceD: "square brackets",
         answer: "C"
-    }
-    //the third question will be [2]
+    },
+    //reference: 
+    //questions.question[2]
+    //questions.choiceA[2] etc
     {
         question: "Arrays in JavaScript can be used to store____.",
-        choiceA: "numbers and strings", 
+        choiceA: "numbers and strings",
         choiceB: "other arrays",
         choiceC: "booleans",
         choiceD: "all of the above",
         answer: "D"
-    }
+    },
 ]
 // Start button
-    function startGame() {
-        isAllDone = false;
-        timerCount = 75;
-        startButton.disabled = true;
-        startTimer()
-        console.log("hi")
-    }
-    
-    function startTimer() {
-        timer=setInterval(function() {
-            timerCount--;
-            timerElement.textContent = timerCount;
+function startGame() {
+    isAllDone = false;
+    timerCount = 75;
+    startButton.disabled = true;
+    startTimer()
+    console.log("hi")
+}
+
+function startTimer() {
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount > 0) {
             if (timerCount > 0) {
-                if (timerCount > 0){
-                    clearInterval(timer);
-                    isAllDone();
-                    console.log(isAllDone);
-                }
-            }
-            if (timerCount === 0){
                 clearInterval(timer);
                 isAllDone();
+                console.log(isAllDone);
             }
-        }, 1000);
-    }
+        }
+        if (timerCount === 0) {
+            clearInterval(timer);
+            isAllDone();
+        }
+    }, 1000);
+}
