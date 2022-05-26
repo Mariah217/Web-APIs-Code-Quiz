aebb6b2e891ee1beda7ac164b34b63b7e6e64810
Mariah <mariahjohnston217@outlook.com>
2022-05-25 22:01:45 -0400

    changed title to h2, changed nav to header and used span for highscore and timer, added section for input initials and save button

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,20 +1,22 @@
 /*
 step 1: display start page, title, paragraph and start button
 step 2: timer will start when start button is clicked
-step 3: create start button to start the game and timer, display question    and 4 answer choices/buttons, start page is hidden, only display question page (after you select an answer, it will go to another page and hide previous question page)
+step 3: create start button to start the game and timer, use addeventlistener, display question and 4 answer choices/buttons, start page is hidden, only display question page (after you select an answer, it will go to another page and hide previous question page)
 step 4.: create if statement (if you answer question correctly, it will present another question and say "correct!")
 step 5: create if statement (if you answer question incorrectly, deduct 10 seconds from timer and say "incorrect")
 step 6: clear interval when all questions are answered (stop the clock)
 step 7: when game is over "all done! your final score is ____." the time remaining on the clock is your final score.
-step 8: create textbox to insert initials at the end of the game
-step 9: create view highscore in top left to view highscore (use local storage)
+step 8: create input textbox to insert initials at the end of the game
+step 9: create view highscore in top left to view highscore (use local storage) show dashboard of all the highscores. When you click submit, should store initial and score in local storage.
 */
 
 //variables
-var startButton = document.querySelector(".start-button");
+var questionsEl = document.querySelector(".questions");
+var introductionEl = document.querySelector("#introduction");
+var startBtn = document.querySelector(".start-button");
 var choices = document.querySelector("#choices");
-var timer;
-var timerCount;
+var timerEl = document.querySelector("#timer");
+var timerCount; 
 var isAllDone = false;
 var finalScore = "";
 var highscores = 0;
@@ -57,25 +59,23 @@ var questions = [
 function startGame() {
     isAllDone = false;
     timerCount = 75;
-    startButton.disabled = true;
+    startButtonEl.disabled = true;
     startTimer()
-    console.log("hi")
+    introductionEl.classList.add("hide");
+    questionsEl.classList.remove("hide");
 }
+
+startBtn.addEventListener("click", startGame);
 
 function startTimer() {
     timer = setInterval(function () {
         timerCount--;
-        timerElement.textContent = timerCount;
-        if (timerCount > 0) {
-            if (timerCount > 0) {
-                clearInterval(timer);
-                isAllDone();
-                console.log(isAllDone);
-            }
-        }
+        console.log(timerCount);
+        timerEl.textContent = timerCount;
+      
         if (timerCount === 0) {
             clearInterval(timer);
-            isAllDone();
+           
         }
     }, 1000);
 }
