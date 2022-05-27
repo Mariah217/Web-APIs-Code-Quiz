30e8f2d7e6ad9bf3a6bae34ae9c44ae2b3d92043
Mariah <mariahjohnston217@outlook.com>
2022-05-26 22:50:21 -0400

    added 3 questions to my js and answers to those 3 questions

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -11,50 +11,50 @@ step 9: create view highscore in top left to view highscore (use local storage) 
 */
 
 //variables
-var highScoreEl=document.querySelector("#high-score");
-var questionEl=document.querySelector("#question");
-var questionContainerEL=document.querySelector("#question-container");
+var highScoreEl = document.querySelector("#high-score");
+var questionEl = document.querySelector("#question");
+var questionContainerEL = document.querySelector("#question-container");
 var answersEl = document.querySelector("#answers");
 var introductionEl = document.querySelector("#introduction");
 var timeEl = document.querySelector("#time");
 var startBtn = document.querySelector(".start-button");
 var saveBtn = document.querySelector("#save");
-var inputInitialsEl=document.querySelector("#input-intials");
+var inputInitialsEl = document.querySelector("#input-intials");
 var dashboardEl = document.querySelector("#dashboard");
-var titleEl=document.querySelector("#title");
-var answer1El=document.querySelector("#answer1");
-var answer2El=document.querySelector("#answer2");
-var answer3El=document.querySelector("#answer3");
-var answer4El=document.querySelector("#answer4");
-var timeRemaining = 75; 
-var index=0;
+var titleEl = document.querySelector("#title");
+var answer1El = document.querySelector("#answer1");
+var answer2El = document.querySelector("#answer2");
+var answer3El = document.querySelector("#answer3");
+var answer4El = document.querySelector("#answer4");
+var timeRemaining = 75;
+var index = 0;
 
 answer1El.addEventListener("click", nextQuestion);
 answer2El.addEventListener("click", nextQuestion);
 answer3El.addEventListener("click", nextQuestion);
-answer4El.addEventListener("click", nextQuestion);
+// answer4El.addEventListener("click",); //add function for input box
 
 startBtn.addEventListener("click", startGame);
-// highScoreEl.addEventListener("click", ) //complete after scoreboard is complete
+// highScoreEl.addEventListener("click", ) //complete after dashboard is complete
 
 
 
-var questions = [ {
-    title: "question 1",
-    answers: ["answer1", "answer2", "answer3", "answer4"],
-    solution: "answer"
+var questions = [{
+    title: "Commonly used data types DO NOT include: ",
+    answers: ["Strings", "Booleans", "Alerts", "Numbers"],
+    solution: "2"
 },
 
 {
-    title: "question 2",
-    answer: ["answer1", "answer2", "answer3", "answer4"],
-    solution: "answer"
+    title: "The condition of an if/else statement is enclosed with___.",
+    answer: ["Quotes", "Curly brackets", "Parenthesis", "Square brackets"],
+    solution: "2"
 },
 
 {
-    title: "question 3",
-    answer: ["answer1", "answer2", "answer3", "answer4"],
-    solution: "answer"
+    title: "Arrays in JavaScript can be used to store___.",
+    answer: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+    solution: "3"
 },
 
 {
@@ -68,34 +68,41 @@ var questions = [ {
     answer: ["answer1", "answer2", "answer3", "answer4"],
     solution: "answer"
 },
+
+
 ]
 
 //time remaining countdown
-function countDown(){
-    timeEl.textContent=timeRemaining;
+function countDown() {
+    timeEl.textContent = timeRemaining;
     timeRemaining--;
 }
 
 
 // Start button, sets clock
-function startGame(){
+function startGame() {
     introductionEl.classList.add("hide");
     questionContainerEL.classList.remove("hide");
     answersEl.classList.remove("hide");
-    clockId=setInterval(countDown, 1000);
+    clockId = setInterval(countDown, 1000);
     displayQuestions();
 
 }
 
-function displayQuestions(){
-        questionEl.textContent=questions[index].title;
-        answer1El.textContent=questions[index].answers[0];
-        answer2El.textContent=questions[index].answers[1];
-        answer3El.textContent=questions[index].answers[2];
-        answer4El.textContent=questions[index].answers[3];
+function displayQuestions() {
+    questionEl.textContent = questions[index].title;
+    answer1El.textContent = questions[index].answers[0];
+    answer2El.textContent = questions[index].answers[1];
+    answer3El.textContent = questions[index].answers[2];
+    answer4El.textContent = questions[index].answers[3];
+
 }
 
-function nextQuestion(){
+function nextQuestion() {
     index++;
     displayQuestions();
 }
+
+// saveBtn.addEventListener('click', function handleClick() {
+//     inputInitialsEl += 'Appended text';
+//   });
